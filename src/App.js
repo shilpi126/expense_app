53b73fa9deed39504ca95ed3e6e3d11680eaa51d
shilpi <shilpi8624@gmail.com>
2024-05-28 22:35:28 +0530

    first custom component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
 
 
 function App() {
   return (
     <div> 
     <h1>Let's get Started</h1>
-    <p>I am learning React</p>
+    <ExpenseItem></ExpenseItem>
     </div>
 );
 }
