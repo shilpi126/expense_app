f57cadbf18b45812fc723f1ceaa621ec2cf52d43
shilpi <shilpi8624@gmail.com>
2024-05-28 22:58:08 +0530

    💡javascript added inside JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import "./ExpenseItem.css";
 const ExpenseItem = () => {
+    const expenseDate = new Date(2023, 7, 15).toISOString();
+    const expenseTitle = "Insurance";
+    const expenseAmount = 50;
+    const expenseLocation = "Bangalore";
+
     return(
     <div>
         <div className="expense-item">
-        <div >15 August 2023</div>
+        <div >{expenseDate}</div>
         <div className="expense-item__description">
-        <h2>Book</h2>
-        <div className="expense-item__price">$10</div>
-        <div className="expense-item__location">Delhi</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+        <div className="expense-item__location">{expenseLocation}</div>
         </div>
         </div>
     </div>
