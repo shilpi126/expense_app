4cb42e5d3ad591cf37589e55f1ae1aa8cfeeaeb4
shilpi <shilpi8624@gmail.com>
2024-05-29 12:21:37 +0530

    fomatting date giving calendar look

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,42 @@
 import React from 'react'
 import "./ExpenseItem.css";
 
-const ExpenseItem = (props) => {
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ]
 
+const ExpenseItem = (props) => {
+    const month = months[props.date.getMonth()];
+    const year = props.date.getFullYear();
+    const date = props.date.getDate();
+    
     return(
-    <div>
-        <div className="expense-item">
-        <div >{props.date.toISOString()}</div>
+    
+        <div key={props.id} className="expense-item">
+        {/* <div >{props.date.toISOString()}</div> */}
+        <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{date}</div>
+        </div>
         <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
         <div className="expense-item__location">{props.location}</div>
         </div>
         </div>
-    </div>
+    
     )
 }
 
