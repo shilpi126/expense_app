3bda365684d4930081531ad1579cf4615ffe622b
shilpi <shilpi8624@gmail.com>
2024-06-05 12:08:29 +0530

    Addind event listener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,6 +6,9 @@ import Card from '../UI/Card';
 
 
 const ExpenseItem = (props) => {
+const buttonClickHandler = () => {
+    console.log("Button Clicked")
+}
 
     return(
     
@@ -14,8 +17,9 @@ const ExpenseItem = (props) => {
         <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
-
+        
         </div>
+        <button onClick={buttonClickHandler}>Change Title</button>
         </Card>
     
     )
